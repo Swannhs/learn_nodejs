ca9fa5328848eca74ab1f098a679f2ee6e1ae00f
Swannhs <princeapurbask@gmail.com>
2021-08-26 02:25:11 +0600

    Added tours by post

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const app = express();
 const port = 4000
+let tours = require('./dev-data/data/tours-simple.json');
 
-const tours = require('./dev-data/data/tours-simple.json');
+app.use(express.json());
 
 app.get('/api/vi/tours', (req, res) => {
     res.status(200).json({
         status: 'success',
+        results: tours.length,
         data: {
             tours: tours
+        }
+    })
+})
+
+app.post('/api/vi/tours', (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id: newId}, req.body)
+    tours += {
+        tours: newTour
+    }
+    res.status(201).json({
+        status: 'success',
+        data: {
+            newTour
         }
     })
 })
