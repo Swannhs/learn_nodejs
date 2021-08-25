c4cf019b5c3c78057a125bed02aabb0a0a3adba3
Swannhs <princeapurbask@gmail.com>
2021-08-25 19:21:38 +0600

    Retrieve data in application and response in json

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const port = 4000
 
+const tours = require('./dev-data/data/tours-simple.json');
+
 app.get('/api/vi/tours', (req, res) => {
-    res.send('Hello world');
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tours: tours
+        }
+    })
 })
 
 app.listen(port, () => {
