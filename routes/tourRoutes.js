168e5452f4cd1b9518555dae9bf22229526ff3bf
Swannhs <princeapurbask@gmail.com>
2021-08-26 23:26:08 +0600

    Calling controller from route

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,51 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const Tour = require('../models/Tours');
-
-
-const getTours = async (req, res) => {
-    try {
-        const tours = await Tour.find();
-        res.status(200).json({
-            success: true,
-            results: tours.length,
-            data: {
-                tours
-            }
-        });
-    } catch (err) {
-        res.status(400).json({message: err})
-    }
-}
-
-const createTour = async (req, res) => {
-    const data = req.body;
-    const tour = new Tour({
-        id: data.id,
-        name: data.name,
-        duration: data.duration,
-        maxGroupSize: data.maxGroupSize,
-        difficulty: data.difficulty,
-        ratingsAverage: data.ratingsAverage,
-        ratingsQuantity: data.ratingsQuantity,
-        price: data.price,
-        summary: data.summary,
-        description: data.description,
-        imageCover: data.imageCover,
-        images: data.images,
-        startDates: data.startDates
-    })
-    try {
-        const saveTour = await tour.save();
-        res.status(201).json(saveTour);
-    } catch (err) {
-        res.status(400).json({message: err})
-    }
-}
+const tourController = require('../controller/tourController');
 
 router
     .route('/')
-    .get(getTours)
-    .post(createTour)
+    .get(tourController.getTours)
+    .post(tourController.createTour)
 
 module.exports = router;
