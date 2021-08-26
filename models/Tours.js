15f9638e3b44bc40d9683600c4bc0f474ed93eab
Swannhs <princeapurbask@gmail.com>
2021-08-27 00:03:05 +0600

    Make name is required

--- models/Tours.js
+++ models/Tours.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 
 const tourSchema = mongoose.Schema({
     id: {
-        type: Number
+        type: Number,
+        unique: true
     },
     name: {
-        type: String
+        type: String,
+        required: true
     },
     duration: {
         type: Number
